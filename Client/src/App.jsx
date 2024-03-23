9e527f73cde61506a3f5df1d2fcf191f9e51b65b
Hasan Maruf Ahmed <hasanmarufahmedfahim@gmail.com>
2024-03-23 22:10:38 +0600

    Added an Error page to handle any unwanted error.

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
 import { Root } from "./routes/Root"
+import { ErrorPage } from "./ErrorPage"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Root />} />
+    <Route path="/" element={<Root />} errorElement={<ErrorPage />} />
   )
 )
 
