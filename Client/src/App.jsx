5eacd116d56a63c5cf1d7883b5e359945446a59c
Hasan Maruf Ahmed <hasanmarufahmedfahim@gmail.com>
2024-03-27 19:37:14 +0600

    Added an index route to display when no other routes are rendered.

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -9,10 +9,12 @@ import { ErrorPage } from "./ErrorPage";
 import Contact, { loader as contactLoader } from "./routes/Contact";
 import EditContact, {action as editAction} from "./routes/EditContact";
 import { action as destroyAction } from "./routes/Destroy";
+import Index from "./routes/Index";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />} errorElement={<ErrorPage />} loader={rootLoader} action={rootAction}>
+      <Route index={true} element={<Index />} />
       <Route path="contacts/:contactId" element={<Contact/>} loader={contactLoader}/>
       <Route path="contacts/:contactId/edit" element={<EditContact />} loader={contactLoader} action={editAction}/>
       <Route path="contacts/:contactId/destroy" action={destroyAction} errorElement={<div>Oops! There was an error.</div>}/>
