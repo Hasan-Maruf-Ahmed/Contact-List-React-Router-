7d12fe45c45db323303366baec7d76ee85563c38
Hasan Maruf Ahmed <hasanmarufahmedfahim@gmail.com>
2024-03-27 22:24:47 +0600

    Synchronized URLs to form state.

--- Client/src/routes/Root.jsx
+++ Client/src/routes/Root.jsx
@@ -7,6 +7,7 @@ import {
   useNavigation,
 } from "react-router-dom";
 import { createContact, getContacts } from "../Contacts";
+import { useEffect } from "react";
 
 export async function action() {
   const contact = await createContact();
@@ -17,12 +18,16 @@ export async function loader({ request }) {
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
   const contacts = await getContacts(q);
-  return { contacts };
+  return { contacts , q };
 }
 
 export const Root = () => {
-  const { contacts } = useLoaderData();
+  const { contacts, q } = useLoaderData();
   const navigation = useNavigation();
+
+  useEffect(() => {
+    document.getElementById("q").value = q;
+  }, [q]);
 
   return (
     <>
@@ -36,6 +41,7 @@ export const Root = () => {
               placeholder="Search"
               type="search"
               name="q"
+              defaultValue={q}
             />
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
