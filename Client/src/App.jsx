70fd1657a9b23c118c820f411dce446c055836bc
Hasan Maruf Ahmed <hasanmarufahmedfahim@gmail.com>
2024-03-27 18:06:51 +0600

    Added delete action for deleting contacts.

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -8,12 +8,14 @@ import { Root, loader as rootLoader, action as rootAction } from "./routes/Root"
 import { ErrorPage } from "./ErrorPage";
 import Contact, { loader as contactLoader } from "./routes/Contact";
 import EditContact, {action as editAction} from "./routes/EditContact";
+import { action as destroyAction } from "./routes/Destroy";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />} errorElement={<ErrorPage />} loader={rootLoader} action={rootAction}>
       <Route path="contacts/:contactId" element={<Contact/>} loader={contactLoader}/>
       <Route path="contacts/:contactId/edit" element={<EditContact />} loader={contactLoader} action={editAction}/>
+      <Route path="contacts/:contactId/destroy" action={destroyAction} errorElement={<div>Oops! There was an error.</div>}/>
     </Route>
   )
 );
