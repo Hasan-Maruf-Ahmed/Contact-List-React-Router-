a030a542a541d292bd283bf17b77fe350422a98e
Hasan Maruf Ahmed <hasanmarufahmedfahim@gmail.com>
2024-03-28 09:29:15 +0600

    Added pathless routes for better showcasing error elements.

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -10,7 +10,10 @@ import {
   action as rootAction,
 } from "./routes/Root";
 import { ErrorPage } from "./ErrorPage";
-import Contact, { loader as contactLoader, action as contactAction } from "./routes/Contact";
+import Contact, {
+  loader as contactLoader,
+  action as contactAction,
+} from "./routes/Contact";
 import EditContact, { action as editAction } from "./routes/EditContact";
 import { action as destroyAction } from "./routes/Destroy";
 import Index from "./routes/Index";
@@ -24,24 +27,26 @@ const router = createBrowserRouter(
       loader={rootLoader}
       action={rootAction}
     >
-      <Route index={true} element={<Index />} />
-      <Route
-        path="contacts/:contactId"
-        element={<Contact />}
-        loader={contactLoader}
-        action={contactAction}
-      />
-      <Route
-        path="contacts/:contactId/edit"
-        element={<EditContact />}
-        loader={contactLoader}
-        action={editAction}
-      />
-      <Route
-        path="contacts/:contactId/destroy"
-        action={destroyAction}
-        errorElement={<div>Oops! There was an error.</div>}
-      />
+      <Route errorElement={<ErrorPage />}>
+        <Route index={true} element={<Index />} />
+        <Route
+          path="contacts/:contactId"
+          element={<Contact />}
+          loader={contactLoader}
+          action={contactAction}
+        />
+        <Route
+          path="contacts/:contactId/edit"
+          element={<EditContact />}
+          loader={contactLoader}
+          action={editAction}
+        />
+        <Route
+          path="contacts/:contactId/destroy"
+          action={destroyAction}
+          errorElement={<div>Oops! There was an error.</div>}
+        />
+      </Route>
     </Route>
   )
 );
